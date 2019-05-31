01c0ca0c83c7e42eb1ea73339a235ef51e8b77ca
idist-hn <minhtoan96.it@gmail.com>
2019-05-31 10:49:23 +0700

    fix register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,25 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="application">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -84,4 +103,9 @@
             </div>
         </div>
     </div>
-@endsection
+
+</div>
+</body>
+<script src="{{ asset('js/app.js') }}"></script>
+
+</html>
